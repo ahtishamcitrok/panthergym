1c84e5a236d8ba84608507bfd6fe68ec335f179c
Qasim-Hafeez <56760084+Qasim-Hafeez@users.noreply.github.com>
2023-10-12 02:27:59 +0500

    tabs js added

--- public/js/custom.js
+++ public/js/custom.js
@@ -89,3 +89,23 @@ jQuery(document).ready(function ($) {
     $('body').toggleClass('menu-open')
   })
 })
+
+
+// schedule page tabs js
+
+$(document).ready(function () {
+  $(".tablinks").click(function () {
+    // Remove the active class from all tablinks
+    $(".tablinks").removeClass("active");
+
+    // Add the active class to the clicked tablink
+    $(this).addClass("active");
+
+    // Hide all tab contents
+    $(".tabcontent").hide();
+
+    // Show the corresponding tab content based on the index of the clicked tablink
+    const index = $(this).index();
+    $(".tabcontent").eq(index).show();
+  });
+});
