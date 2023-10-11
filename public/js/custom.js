7db5c32015fa5e35ef8e1a286c730b639655ab48
ahtishamcitrok <ahtisham.ali@citrok.com>
2023-10-12 02:17:01 +0500

    Ahtisham Changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,21 +1,91 @@
-const swiper = new Swiper('.products-list', {
-    // Optional parameters
-    direction: 'vertical',
+jQuery(document).ready(function ($) {
+
+  // Products Slider 
+  $('.products-list').owlCarousel({
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 4,
+        nav: true,
+        loop: true
+      }
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    nav: true,
+    navText: [
+        "<img src='./img/arrow-left.svg' class='prev-btn'>",
+        "<img src='./img/arrow-right.svg' class='next-btn'>"
+    ],
+    onInitialized: setEqualHeight, // Call function on carousel initialization
+    onResized: setEqualHeight,
+  })
+
+  // Testimonails Slider 
+  $('.review-parent-box').owlCarousel({
+    loop: true,
+    margin: 24,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 2,
+        nav: true
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: true
+      }
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    nav: true,
+    navText: [
+        "<img src='./img/arrow-left.svg' class='prev-btn'>",
+        "<img src='./img/arrow-right.svg' class='next-btn'>"
+    ],
+    onInitialized: setEqualHeight, // Call function on carousel initialization
+    onResized: setEqualHeight,
+    
+  })
+  function setEqualHeight(event) {
+    var maxHeight = 0;
+    $('.owl-item', event.target).each(function () {
+      var itemHeight = $(this).height();
+      maxHeight = Math.max(maxHeight, itemHeight);
+    });
+    $('.owl-item', event.target).height(maxHeight);
+  }
+
+  // Mobile Menu Toggle
+  if ($(window).width() <= 991) {
+    $('.header-nav').prepend('<img src="./img/close-icon.svg" class="close-menu" alt="">')
+  }
+
+  var $headerNav = $('.header-nav, .menu-toggle');
+
+  // Click event handler on the document
+  $(document).on('click', function (event) {
+    // Check if the clicked element is not within .header-nav and its children
+    if (!$(event.target).closest($headerNav).length) {
+      // Toggle the class on the hidden element
+      $('.header-nav').removeClass('visible')
+      $('body').removeClass('menu-open')
+    }
   });
+
+  $('.menu-toggle, .close-menu').on('click', function () {
+    $('.header-nav').toggleClass('visible')
+    $('body').toggleClass('menu-open')
+  })
+})
